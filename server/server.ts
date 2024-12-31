d8301e6edc0a48f1c777d1cdbc81c5f9ac94fff5
duck4i <mihailogazda@gmail.com>
2024-12-31 13:25:19 +0000

    api docs

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,12 @@
 import { serve, type ServerWebSocket } from "bun";
 import { Hono } from "hono";
-import { serveStatic } from "hono/serve-static";
+import { apiReference } from "@scalar/hono-api-reference";
+import { openAPISpecs, describeRoute } from "hono-openapi";
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
 import log from "./log";
 
 const httpPort = 8080;
-
 const app = new Hono();
 
 //  --------------------------------------------------------
@@ -16,8 +18,8 @@ type WMessage = string | Buffer;
 const clients = new Set<WSocket>();
 
 const onConnect = (ws: WSocket) => {
-    log.info(`User connected: ${ws.remoteAddress}`);
     clients.add(ws);
+    log.info(`User connected: ${ws.remoteAddress}. (${clients.size})`);
 }
 
 const onDisconnect = (ws: WSocket) => {
@@ -37,15 +39,28 @@ const onMessage = async (ws: WSocket, message: WMessage) => {
 //  Web
 //  --------------------------------------------------------
 
-const readFile = async (path: string): Promise<string | null> => {
-    try {
-        const file = await Bun.file(path).text();
-        return file;
-    } catch (err) {
-        log.error(`Error reading file: ${path}`, err);
-        return null;
+//  Docs and status 
+
+const documentSuccessOrFailForType = (obj: z.AnyZodObject): any => {
+    return {
+        200: { description: "Success", content: { 'application/json': { schema: (zodToJsonSchema(obj) as any) } } },
+        500: { description: "Error" }
     }
 }
+
+app.get('/openapi', openAPISpecs(app, {
+    documentation: {
+        info: { title: 'Sloppy Chat API', version: '1.0.0', description: 'Simple websocket based chat server and client' },
+        servers: [{ url: `http://localhost:${httpPort}`, description: 'Local Server' }],
+    },
+}));
+
+app.get('/docs', apiReference({
+    theme: 'saturn',
+    spec: { url: '/openapi' },
+}));
+
+//  Routes
 
 app.onError((err, c) => {
     log.error(err);
@@ -53,28 +68,35 @@ app.onError((err, c) => {
     return c.json({ message: err.message, cause: err.cause, stack: err.stack });
 });
 
-app.get("/status", async (c) => {
-    log.debug("Status OK");
-    return c.json({ 
-        status: "OK",
-        clients: clients.size 
-    });
+const StatusResponse = z.object({
+    clients: z.number().describe("Number of connected clients")
 });
+
+app.get("/status",
+    describeRoute({
+        description: "Server status",
+        responses: documentSuccessOrFailForType(StatusResponse)
+    }),
+    async (c) => {
+        log.debug("Status OK");
+        const resp: z.infer<typeof StatusResponse> = {
+            clients: clients.size
+        };
+        return c.json(resp);
+    }
+);
 
 app.get('/chat/:id', async (c) => {
     const id = c.req.param("id");
     return c.json({ id: id });
 });
 
-app.get("/", (c) => {
-    log.debug("Root page request");
-    return c.text("Hello to Sloppy Chat");
-});
-
-app.use("/client/test", serveStatic({
-    root: "./public",
-    getContent: (p, c) => readFile("./public/test_client.html")
-}));
+app.get("/",
+    (c) => {
+        log.debug("Root page request");
+        return c.html("Hello to Sloppy Chat. Click here for <a href='/docs'> API </a>.");
+    }
+);
 
 //  --------------------------------------------------------
 //  Server boostrap
