4a64ccd6b92e0214b2b4ba18c0e45ed897d9ab3b
duck4i <mihailogazda@gmail.com>
2025-01-06 17:53:56 +0000

    cleanup

--- docker/index.ts
+++ docker/index.ts
@@ -1,21 +1,4 @@
-import { bots, startServerWithUI, type BotProcessReturn, type BotReply } from "@duck4i/sloppy-chat-full";
-import type { ServerWebSocket } from "bun";
+import { startServerWithUI } from "@duck4i/sloppy-chat-full";
 
-const addBot = () => {
-    const examplePingBot = async (message: string, userName: string, userId: string): BotProcessReturn => {
-        if (message === "!slop") {
-            const rp: BotReply = {
-                botName: "~SloppyPong~",
-                message: "SLOOOP!",
-                onlyToSender: false
-            };
-            return rp;
-        }
-        return null;
-    }
-    bots.push(examplePingBot);
-}
-
-addBot();
 
 const server = startServerWithUI();
