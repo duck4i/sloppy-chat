305af2c784fc7de83a093580134d15ae5f0d1581
duck4i <mihailogazda@gmail.com>
2025-01-06 18:51:49 +0000

    implemented props for IPs

--- packages/full/src/client.tsx
+++ packages/full/src/client.tsx
@@ -1,8 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import Chat from './chat';
+import { Chat } from './chat';
+
+const initialProps = (window as any).__INITIAL_PROPS__ || {}
 
 const container = document.getElementById('app');
 if (container) {
     const root = createRoot(container);
-    root.render(<Chat />);
+    root.render(<Chat {...initialProps} />);
 }
